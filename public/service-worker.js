2b59799be918f268c1176fd7b78489d04d280590
Appeltauermedia <info@appeltauermedia.com>
2026-08-01 15:33:20 +0200

    Mobiles Coverlayout korrigieren

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "ltv-1.1.0";
+const VERSION = "ltv-1.1.1";
 const STATIC = `${VERSION}-static`;
 const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./favicon.ico","./images/lt-cover-front.webp","./icons/favicon-32.png","./icons/favicon-48.png","./icons/icon.svg","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(STATIC).then(cache => cache.addAll(APP_SHELL))));
